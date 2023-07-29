fb66fd462539b22ef07cbc842b65273dbcc4499e
namt93 <namt7115633@gmail.com>
2023-07-29 22:40:40 +0700

    Set up static file & SCSS

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const app = express();
 const port = 3000;
 const handlebars = require("express-handlebars");
 
+app.use(express.static(path.join(__dirname, "public")));
+
 // HTTP logger
 app.use(morgan("combined"));
 
