134ef12728ff64550a3caa6382924f0ed987d81d
namt93 <namt7115633@gmail.com>
2023-07-31 23:56:25 +0700

    Setup routes & controllers

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const app = express();
 const port = 3000;
 const handlebars = require("express-handlebars");
+const route = require("./routes");
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
@@ -19,16 +20,12 @@ app.engine(
     extname: ".hbs",
   })
 );
+
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-app.get("/", (req, res) => res.render("home"));
-app.get("/news", (req, res) => res.render("news"));
-app.get("/search", (req, res) => res.render("search"));
-app.post("/search", (req, res) => {
-  console.log(req.body);
-  res.send("");
-});
+// Routes init
+route(app);
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
