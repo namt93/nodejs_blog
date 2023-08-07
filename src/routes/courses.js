81e3c4fcfbe092318e501124860dce0197053c7f
namt93 <namt7115633@gmail.com>
2023-08-07 17:49:53 +0700

    Create delete router

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -7,6 +7,7 @@ router.get("/create", courseController.create);
 router.post("/add", courseController.add);
 router.get("/:id/edit", courseController.edit);
 router.put("/:id", courseController.update);
+router.delete("/:id", courseController.destroy);
 router.get("/:slug", courseController.show);
 
 module.exports = router;
